303107dcf92afa0fcd682d059dafa7bf14e3a518
Rajeshwarikota <kotarajeswari003.com>
2023-04-15 18:07:20 +0530

    [Rajeshwari kota] Adding Indian state Census Analyzer problem

---
+++ IndianStateCensusProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("....Indian State Census Problem.....");
+            Console.ReadLine();
+        }
+    }
+}
